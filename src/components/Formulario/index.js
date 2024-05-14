3d3406d427847528e75c837eb913af7a2bda44e6
Adriano <adrianosenny@gmail.com>
2024-05-14 15:53:20 -0300

    Refactor: Adicionado estados aos componentes (useState)

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,13 +2,19 @@ import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
+import { useState } from "react";
 
 const Formulario = () => {
   const times = ["Vôlei", "Basquete", "League of Legends"];
 
+  const [nome, setNome] = useState("");
+  const [posicao, setPosicao] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("foi");
+    console.log("foi", nome, posicao, imagem, time);
   };
 
   return (
@@ -19,18 +25,30 @@ const Formulario = () => {
           obrigatorio={true}
           label="Nome"
           placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Posição"
           placeholder="Digite sua Posição"
+          valor={posicao}
+          aoAlterado={(valor) => setPosicao(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Imagem"
           placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
         />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>
