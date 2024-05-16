dd3990514899d360d7b26b19145e526543c9c41e
Adriano <adrianosenny@gmail.com>
2024-05-16 15:42:21 -0300

    Style: Adicionando fundo ao componente "Time"

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -2,21 +2,28 @@ import Membro from "../Membro";
 import "./Time.css";
 
 const Time = (props) => {
-  const css = { backgroundColor: props.corPrimaria };
   return (
     props.membros.length > 0 && (
-      <section className="time" style={css}>
+      <section
+        className="time"
+        style={{
+          backgroundColor: props.corPrimaria,
+          backgroundImage: "url(/images/fundo.png)",
+        }}
+      >
         <h3 style={{ borderColor: props.corSecundaria }}>{props.nome}</h3>
         <div className="membros">
-          {props.membros.map((membro) => (
-            <Membro
-              key={membro.nome}
-              nome={membro.nome}
-              posicao={membro.posicao}
-              imagem={membro.imagem}
-              cor={props.corSecundaria}
-            />
-          ))}
+          {props.membros.map((membro, indice) => {
+            return (
+              <Membro
+                key={indice}
+                nome={membro.nome}
+                posicao={membro.posicao}
+                imagem={membro.imagem}
+                cor={props.corSecundaria}
+              />
+            );
+          })}
         </div>
       </section>
     )
